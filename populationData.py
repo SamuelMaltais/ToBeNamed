f8e2809ae9d801f22ce7dede7604830bf5dfa93b
Rihazul <rihazulislam@gmail.com>
2024-01-20 16:36:55 -0500

    .

--- populationData.py
+++ populationData.py
@@ -1,28 +1,36 @@
-import csv
+import pandas as pd 
+from pathlib import Path 
+import json
+from flask import Flask, Response
 
-def getPopulationPerHood():
 
-    # TODO be adapted to pull from https://donnees.montreal.ca/dataset/population-recensement/resource/8dbac698-14c6-4c91-9ba8-85fdb3232c45
+app = Flask(__name__)
+
+path = Path(r"C:\Users\rihaz\Dropbox\PC\Downloads\population-quartiers-anciens-territoires-administratifs.csv")
+
+def read_csv_to_json(csv_file_path):
+    
+    df_file = pd.read_csv(path)
+    df_file = df_file.drop(df_file.columns[[1]],axis =1)
+    df_file = df_file.drop([df_file.index[0],df_file.index[3],df_file.index[2],df_file.index[67],df_file.index[68],df_file.index[69],df_file.index[70]])
+    df_file = df_file.rename({'Unnamed: 0':'Quartiers de MONTRÉAL'},axis=1)
+    print(df_file)
+    json_data = df_file.to_json(orient="records")
+    
+    return json_data
+
+@app.route('/get_data')
+
+def get_data():
+    csv_file_path = r"C:\Users\rihaz\Dropbox\PC\Downloads\population-quartiers-anciens-territoires-administratifs.csv"
+    json_data = read_csv_to_json(csv_file_path) 
+    print(json.dumps(json_data))
+    
+    return Response(json.dumps(json_data),content_type= "application/json")
+
+if __name__ == "__main__":
+    app.run(debug=True)
 
 
-    with open('employee_birthday.txt') as csv_file:
 
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        line_count = 0
-        for row in csv_reader:
-            if line_count == 0:
-                print(f'Column names are {", ".join(row)}')
-                line_count += 1
-            else:
-                print(f'\t{row[0]} works in the {row[1]} department, and was born in {row[2]}.')
-                line_count += 1
-
-    print(f'Processed {line_count} lines.')
-
-    # TODO
-    # You can return here the object with the intended format which is gonna look like
-    # "neighborhood-name" : {
-    #     "population": num
-    # }   
-    return
 
