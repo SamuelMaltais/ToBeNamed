7657f36b319f9524605a5d899042fd50fb047977
samuel <leumasmontreal@hotmail.com>
2024-01-20 16:42:31 -0500

    .

--- main.py
+++ main.py
@@ -1,29 +1,25 @@
 import populationData
 import bikeData
 import treeData
-
+import json
 from flask import Flask, Response
 
 
 app = Flask(__name__)
+ourDictionnary = {}
 
 @app.route('/get_data')
 def get_data():
-    json_data = read_csv_to_json(csv_file_path) 
-    print(json.dumps(json_data))
-    
-    return Response(json.dumps(json_data),content_type= "application/json")
 
-
+    return Response(json.dumps(ourDictionnary),content_type= "application/json")
 
 
 
 if __name__ == "__main__":
-
-
+    
     ourDictionnary = populationData.getPopulationPerHood()
     bikeData.getBikeData(ourDictionnary)
     treeData.getTreeData(ourDictionnary)
 
-
+    app.run(debug=True)
     pass
